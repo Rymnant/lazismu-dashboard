f91c58083be2348498f40d89de7f140127a0dfaf
Rymnant <frdrchmn.school@gmail.com>
2024-10-25 03:44:08 +0700

    Add new components: DatabaseContent

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -1,8 +1,5 @@
+import DatabaseContent from '../../components/DatabaseContent'
+
 export default function DatabasePage() {
-    return (
-      <main className="p-6">
-        <h1 className="text-2xl font-semibold">Database</h1>
-        {/* Add database content here */}
-      </main>
-    )
+  return <DatabaseContent />
   }
