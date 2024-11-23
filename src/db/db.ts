93851e829e77e76c3a42644e7c89cb4160f1cb21
Rachman <159479173+Rymnant@users.noreply.github.com>
2024-11-23 23:14:50 +0700

    Remove some database configuration

--- src/db/db.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/db.ts
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'database.sqlite',
-});
-
-export default sequelize;
--- src/lib/db.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/db.ts
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'database.sqlite', // Atur lokasi database Anda
-});
-
-export default sequelize;
